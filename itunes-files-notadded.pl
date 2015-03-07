2d609abc815f507bb3b4cd40e7cd193efb814469
Kevin Spencer <kevin@kevinspencer.org>
2015-03-07 13:49:12 -0700

    now decoding entities

--- itunes-files-notadded.pl
+++ itunes-files-notadded.pl
@@ -14,36 +14,37 @@
 use Data::Dumper;
 use Digest::MD5 qw(md5_hex);
 use File::Find::Rule;
-use File::Spec;
+use HTML::Entities;
 use URI::Escape;
 use strict;
 use warnings;
 
 $Data::Dumper::Indent = 1;
 
-our $VERSION = '0.1';
+our $VERSION = '0.2';
 
-my $BASEDIR = '/Users/kevin/Music/iTunes';
-my $FILEDIR = File::Spec->catfile($BASEDIR, 'iTunes Music');
-my $XMLFILE = File::Spec->catfile($BASEDIR, 'iTunes Music Library.xml');
-
-# <key>Location</key><string>file://localhost/Users/kevin/Music/iTunes/iTunes%20Music/a-Ha/Hunting%20High%20And%20Low/06%20The%20Sun%20Always%20Shines%20On%20T.V..mp3</string>
+my $BASEDIR = '/Users/' . getpwuid($<) . '/Music/iTunes';
+my $FILEDIR = $BASEDIR . '/iTunes Music';
+my $XMLFILE = $BASEDIR . '/iTunes Music Library.xml';
 
 my $files_in_library = {};
 open(my $fh, '<', $XMLFILE) || die "Could not open $XMLFILE - $!\n";
 while (<$fh>) {
     my $line = $_;
     if ($line =~ /<key>Location<\/key><string>file:\/\/localhost(.+)<\/string>/) {
-        my $location_hash = md5_hex(uri_unescape($1));
+        my $location_hash = md5_hex(decode_entities(uri_unescape($1)));
         $files_in_library->{$location_hash} = 1;
     }
 }
 close($fh);
 
+my $count = 0;
 for my $file (File::Find::Rule->file()->name('*.mp3','*.m4a')->in($BASEDIR)) {
     my $filename_on_disk_hash = md5_hex($file);
     if (! $files_in_library->{$filename_on_disk_hash}) {
-        print $file, "\n";
-        exit();
+        print "!!!$file\n";
+        $count++;
     }
 }
+
+print "\nFound $count songs on disk not in the library\n";
